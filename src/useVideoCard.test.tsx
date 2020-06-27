be624083c70009e7ae5a599d41308fe60646b749
Matt <kapakapa197@gmail.com>
2020-06-27 22:09:27 +0200

    Small adjustments

--- src/useVideoCard.test.tsx
+++ src/useVideoCard.test.tsx
@@ -5,3 +5,7 @@ import ctx from 'jest-webgl-canvas-mock'
 test("should return graphics information", () => {
     ctx.beginPath();
 })
+
+// TODO: Unit testing
+// Currently i have not find a proper way to actually test this package,
+// may fix in the future

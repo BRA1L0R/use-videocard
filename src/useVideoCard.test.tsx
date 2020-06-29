07c1b168d64942bc4a25b5ee119bda7278f800ee
Matt <kapakapa197@gmail.com>
2020-06-29 12:03:40 +0200

    Update README.md

--- src/useVideoCard.test.tsx
+++ src/useVideoCard.test.tsx
@@ -1,11 +1,3 @@
-import { renderHook, act } from '@testing-library/react-hooks'
-import { useVideoCard } from './useVideoCard'
-import ctx from 'jest-webgl-canvas-mock'
-
-test("should return graphics information", () => {
-    ctx.beginPath();
-})
-
 // TODO: Unit testing
 // Currently i have not find a proper way to actually test this package,
 // may fix in the future
